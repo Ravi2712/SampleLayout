33fa3a274e684ab83fabc35035e1ea740474fbb4
rvpatel <patel.ravi2712@gmail.com>
2016-02-25 15:26:02 -0800

    Initial Commit

---
+++ SampleLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleLayout
+//
+//  Created by Ravi on 2/25/16.
+//  Copyright © 2016 JayPatel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleLayout/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleLayout
+//
+//  Created by Ravi on 2/25/16.
+//  Copyright © 2016 JayPatel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
